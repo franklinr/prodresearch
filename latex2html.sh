dfd25b26b09210f8e1d77ba4fb67ab343b9bf77e
Chang <chang@H20015.local>
2020-09-08 21:22:02 +0900

    Add existing file

--- latex2html.sh
+++ latex2html.sh
@@ -3,3 +3,7 @@ bibtex main
 htlatex main
 perl -0777 -pe 's/,\s+[&][#]8212;[&][#]8212;-/ &nbsp;&nbsp;&nbsp;&nbsp/g' main.html > main2.html
 
+rm main.[^ht]* main.tmp 
+git add .
+git commit -m "Add existing file"
+git push origin
